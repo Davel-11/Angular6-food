bb8437c13a910ea093452773265d3ca7c4378b29
Davel-11 <davelaltan@gmail.com>
2019-02-08 21:01:19 -0500

    finishing enslish on gt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent  {
 
   closeMoreMenu(){
     this.hideMoreOptions.hideMoreOptionsEvent();
-    this.hideMoreOptions.hideshowMenuPhone();
+    this.hideMoreOptions.hideshowMenuPhone();   
   }
 
 }
